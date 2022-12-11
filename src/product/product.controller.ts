1881e3b453ebc43ee4868498d1de0889f14b9c62
WebCONuz <muxammadi0799@gmail.com>
2022-12-11 19:23:10 +0500

    Finished CRUD for product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,5 +1,14 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 import { ProductService } from './product.service';
 
 @Controller('product')
@@ -10,5 +19,29 @@ export class ProductController {
   @Post()
   create(@Body() createProductDto: CreateProductDto) {
     return this.productService.create(createProductDto);
+  }
+
+  // Get all Product Controller
+  @Get()
+  getAll() {
+    return this.productService.getAll();
+  }
+
+  // Get one Product Controller
+  @Get(':id')
+  getOne(@Param('id') id: number) {
+    return this.productService.getOne(+id);
+  }
+
+  // Update Product Controller
+  @Put(':id')
+  update(@Param('id') id: number, @Body() updateProductDto: UpdateProductDto) {
+    return this.productService.update(+id, updateProductDto);
+  }
+
+  // Delete Product Controller
+  @Delete(':id')
+  delete(@Param('id') id: number) {
+    return this.productService.delete(+id);
   }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 import { Product } from './product.model';
 
 @Injectable()
@@ -12,5 +13,31 @@ export class ProductService {
   // Create Product Service
   async create(createProductDto: CreateProductDto) {
     return this.productRepository.create(createProductDto);
+  }
+
+  // Get Peoducts Service
+  async getAll() {
+    return this.productRepository.findAll();
+  }
+
+  // Get one Peoduct Service
+  async getOne(id: number) {
+    return this.productRepository.findOne({
+      where: { id },
+      include: { all: true },
+    });
+  }
+
+  // Update Peoduct Service
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    return this.productRepository.update(updateProductDto, {
+      where: { id },
+      returning: true,
+    });
+  }
+
+  // Delete Peoduct Service
+  async delete(id: number) {
+    return this.productRepository.destroy({ where: { id } });
   }
 }
