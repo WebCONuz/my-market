7a3a056dc4ce6b6ce99f3a7136fd706d968e0daa
WebCONuz <muxammadi0799@gmail.com>
2022-12-11 10:20:43 +0500

    Build Project

---
+++ src/app.module.ts
@@ -0,0 +1,36 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MarketModule } from './market/market.module';
+import { BranchModule } from './branch/branch.module';
+import { ProductModule } from './product/product.module';
+import { WorkerModule } from './worker/worker.module';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [
+    // Read environment variables
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+
+    // Connect to PostgreSQL database
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: process.env.POSTGRES_HOST,
+      port: Number(process.env.POSTGRES_PORT),
+      username: process.env.POSTGRES_USER,
+      password: String(process.env.POSTGRES_PASSWORD),
+      database: process.env.POSTGRES_DB,
+      models: [],
+      autoLoadModels: true,
+      logging: false,
+    }),
+
+    // Custom modules
+    MarketModule,
+    BranchModule,
+    ProductModule,
+    WorkerModule,
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  // Created main module
+  const app = await NestFactory.create(AppModule);
+
+  // Use Validate
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Run Project
+  const Port = process.env.PORT || 4001;
+  await app.listen(Port, () => {
+    console.log(`Loyiha ${Port}-portda ishga tushdi.`);
+  });
+}
+bootstrap();
